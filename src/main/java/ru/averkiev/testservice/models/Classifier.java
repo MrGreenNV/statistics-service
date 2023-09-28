56bf57ba688f0fcbb147fac5a66768e28204d24a
MrGreenNV <averkievnv@gmail.com>
2023-09-28 15:45:33 +0300

    add Classifier.java добавлен класс классификатора

---
+++ src/main/java/ru/averkiev/testservice/models/Classifier.java
@@ -0,0 +1,26 @@
+package ru.averkiev.testservice.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Класс описывает модель классификатора.
+ * @author mrGreenNV
+ */
+@Entity
+@Table(name = "classifiers")
+@Getter
+@Setter
+public class Classifier extends BaseEntity {
+
+    /** Тип классификатора */
+    @Column(name = "type")
+    private String type;
+
+    /** Описание типа классификатора */
+    @Column(name = "description")
+    private String description;
+}
