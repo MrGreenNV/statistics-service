8e9c462efaafdd168e0cfcfc583e6eeefb7920eb
MrGreenNV <averkievnv@gmail.com>
2023-09-28 18:01:44 +0300

    update EventRepository.java, pom.xml add EventDTO.java, EventCreateDTO.java добавлены DTO для событий

--- src/main/java/ru/averkiev/testservice/repositories/EventRepository.java
+++ src/main/java/ru/averkiev/testservice/repositories/EventRepository.java
@@ -1,11 +1,21 @@
 package ru.averkiev.testservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.averkiev.testservice.models.Event;
+
+import java.util.List;
 
 /**
  * Репозиторий для взаимодействия объектов класса Event с базой данных.
  * @author mrGreenNV
  */
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    /**
+     * Выполняет поиск всех событий в базе данных и сортирует их по дате в порядке убывания.
+     * @return отсортированный список всех событий.
+     */
+    List<Event> findAllByOrderByHappenedAtDesc();
 }
